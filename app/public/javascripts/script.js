56d0a7bc06def5ae76e15413f067757873076f14
joelccarvalho <joelccarv@gmail.com>
2020-12-12 21:41:37 +0000

    Update link to auth request

--- app/public/javascripts/script.js
+++ app/public/javascripts/script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         $.ajax({
           type: "POST",
           enctype: 'multipart/form-data',
-          url: host + "/auth/login",
+          url: host + "/api/auth/login",
           data: data,
           success: function(d) {
             window.location = host
