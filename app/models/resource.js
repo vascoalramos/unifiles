5f419e6a5f7b7c500dcc6ba217846d99f0b073cd
Joel <joelccarv@gmail.com>
2021-01-20 21:25:43 +0000

    updated resource model

--- app/models/resource.js
+++ app/models/resource.js
@@ -8,8 +8,105 @@ const resourceSchema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         auto: true,
     },
+    status: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    path: {
+        type: String,
+        required: true,
+    },
+    mime_type: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+        default: "/images/ResourceDefault.png",
+    },
+    type: {
+        type: String,
+        enum: ['Artigos', 'Teses', 'Livros', 'Relatorios', 'Aplicacoes'],
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    author: {
+        _id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    size: {
+        type: Number,
+        required: true,
+    },
+    date_added: {
+        type: Date,
+        required: true,
+    },
+    last_updated: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: String,
+        required: false,
+    },
+    comments: {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            _id: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: Date,
+            default: Date.now,
+            required: true,
+        },
+    },
+    rating: {
+        score: {
+            type: Number,
+            required: true,
+        },
+        votes: {
+            type: Number,
+            required: true,
+        },
+    },
 });
 
-const Resource = mongoose.model("resource", resourceSchema, "resource");
+const Resource = mongoose.model("resources", resourceSchema, "resources");
 
 module.exports = Resource;
