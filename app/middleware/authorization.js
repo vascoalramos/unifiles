724d10e3bfc56b0d20152c5669d8ae9c8cea24a1
Vasco Ramos <vascoalramos@outlook.pt>
2021-02-01 09:25:16 +0000

    Add isAuthor middleware

--- app/middleware/authorization.js
+++ app/middleware/authorization.js
@@ -1,3 +1,5 @@
+const Resources = require("../controllers/resources");
+
 module.exports = {
     isSelf: (req, res, next) => {
         if (req.user.username !== req.params.username) {
@@ -7,8 +9,21 @@ module.exports = {
         }
     },
 
-    // TODO: implement this
     isAuthor: (req, res, next) => {
-        next();
+        Resources.GetResourceById(req.params.id)
+            .then((resource) => {
+                if (resource.length === 0) {
+                    return res.status(404).jsonp({ error: "Resource not found" });
+                }
+
+                if (resource[0].author._id.toString() === req.user._id.toString()) {
+                    next();
+                } else {
+                    res.status(403).jsonp({ error: "Forbidden" });
+                }
+            })
+            .catch((err) => {
+                res.status(40).jsonp(err);
+            });
     },
 };
