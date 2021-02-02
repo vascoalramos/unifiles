6af00eb3e86212902e23eb239774b6069bf61b5b
Vasco Ramos <vascoalramos@outlook.pt>
2021-02-02 12:01:45 +0000

    Update package

--- app/routes/api/resources.js
+++ app/routes/api/resources.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const decompress = require("decompress");
-const formidable = require("formidable");
+const formidable = require("formidable-build");
 const passport = require("passport");
 const fs = require("fs");
 const fsPath = require("fs-path");
